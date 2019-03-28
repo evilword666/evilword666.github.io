4c4f740fc73e89c51c010255e51120c92f1baace
Javier Avelino <evilword.com@gmail.com>
2019-03-28 12:14:26 -0600

    Proyecto de clima

---
+++ precache-manifest.e39525a96149622d0df29be563142b76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "313c3397d9d30210d31b",
+    "url": "/static/js/main.49a71261.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "313c3397d9d30210d31b",
+    "url": "/static/css/main.74b8a09c.chunk.css"
+  },
+  {
+    "revision": "d3e68c4f3365758a2c8e35d3e74eb313",
+    "url": "/index.html"
+  }
+];
